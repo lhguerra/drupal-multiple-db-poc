b8231e59afa063ab62b1c5232b3179456a13cd24
Luiz Henrique Guerra <lhguerra@gmail.com>
2020-06-11 21:07:22 -0300

    chore: Override node storage handler

---
+++ web/modules/custom/multiple_db/src/NodeStorage.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\multiple_db;
+
+use Drupal\node\NodeStorage as CoreNodeStorage;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Defines the storage handler class for nodes.
+ *
+ * This extends the base storage class, adding required special handling for
+ * node entities.
+ */
+class NodeStorage extends CoreNodeStorage
+{
+  public static function createInstance(ContainerInterface $container, EntityTypeInterface $entityType)
+  {
+    return new static(
+      $entityType,
+      $container->get('multiple_db.database.second'),
+      $container->get('entity.manager'),
+      $container->get('cache.entity'),
+      $container->get('language_manager'),
+      $container->get('entity.memory_cache')
+    );
+  }
+}
