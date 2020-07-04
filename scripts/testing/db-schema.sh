0eaf1f8a8e1448a18b127eb4e450809d69c3d622
Luiz Henrique Guerra <lhguerra@gmail.com>
2020-07-04 17:57:29 -0300

    pivot node poc

--- scripts/testing/db-schema.sh
+++ scripts/testing/db-schema.sh
@@ -3,4 +3,4 @@
 set -e
 
 docker-compose run --rm app \
-  bash -c "drush ev 'drop_database_tables();' && drush ev 'drupal_set_installed_schema_version('multiple_db', 8000)' && drush updb -y"
+  bash -c "drush en multiple_db -y && drush ev 'drop_database_tables();' && drush ev 'drupal_set_installed_schema_version('multiple_db', 8000)' && drush updb -y -v"
